083a436246a0b8c32b75f273132753afc5ab1c11
bay3s <sirbay3s@gmail.com>
2023-05-09 17:12:32 +0200

    checkpoint

--- shell/run_bandits.sh
+++ shell/run_bandits.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH -t 2:30:00
-#SBATCH -n 10
+#SBATCH --time 2:30:00
+#SBATCH --ntask
 #SBATCH --gpus=1
 
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=sirbay3s@gmail.com
 
-# load modules
-module load 2021
-module load Python/3.9.5-GCCcore-10.3.0
+# activate env
+source /home/${USER}/.bashrc
+source activate mujoco_env
 
-conda activate mujoco_env
-python3 $HOME/rl-squared/rl_squared/runs/bandits/run.py --n=10 --k=10
+# slurm run
+srun $HOME/rl-squared/runs/bandits/run.py --n=10 --k=10
