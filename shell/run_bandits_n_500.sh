27b24cfcfcc8ece59c6d5884cdad2c432eb5e70f
bay3s <sirbay3s@gmail.com>
2023-05-16 11:11:15 +0200

    setup bandit experiments

---
+++ shell/run_bandits_n_500.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --partition=thin
+#SBATCH --time=2:00:00
+
+source /home/${USER}/.bashrc
+source activate mujoco_env
+
+srun python3 $HOME/rl-squared/runs/bandits/run.py --n=500 --k=5
+srun python3 $HOME/rl-squared/runs/bandits/run.py --n=500 --k=10
+srun python3 $HOME/rl-squared/runs/bandits/run.py --n=500 --k=50
