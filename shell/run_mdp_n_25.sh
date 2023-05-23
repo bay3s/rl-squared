6b5d130409fa708d01b674706a2ddc74c3412115
bay3s <sirbay3s@gmail.com>
2023-05-23 14:12:07 +0200

    add mdp shells

---
+++ shell/run_mdp_n_25.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --time=4:00:00
+
+source /home/${USER}/.bashrc
+source activate rl
+
+srun python3 $HOME/rl-squared/runs/ant/run.py --n=25 --prod
